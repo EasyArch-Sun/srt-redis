f6affd03fcf3af68b4001dfbf19723d0c27ef76a
EasyArch-Sun <3268543958@qq.com>
2020-10-11 23:09:38 +0800

    redis-server-test

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList=new LinkedList<>();
+        Map<String,String> map=new HashMap<>();
+        String put=map.put("1","1");
+        System.out.println(map.get("1"));
+
+    }
+}
